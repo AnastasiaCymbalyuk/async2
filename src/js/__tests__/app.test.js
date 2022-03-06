d0ded01b5a4485a49c0e237c2d81b64ae78ad3d8
AnastasiaCymbalyuk <n.wolk@mail.ru>
2022-03-06 13:33:02 +0300

    async2(fixed)

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,12 +1,14 @@
-import GameSavingLoader from '../loader';
+import GameSavingLoader, { GameSaving } from '../loader';
 
 it('test', async () => {
-  const save = await GameSavingLoader.load();
-  expect(save).toEqual({
+  const obj = JSON.stringify({
     id: 9,
     created: 1546300800,
     userInfo: {
       id: 1, name: 'Hitman', level: 10, points: 2000,
     },
   });
+  const objNew = new GameSaving(obj);
+  const save = await GameSavingLoader.load();
+  expect(save).toEqual(objNew);
 });
--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,18 @@
+// eslint-disable-next-line max-classes-per-file
 import read from './reader';
 import json from './parser';
+
+export class GameSaving {
+  constructor(obj) {
+    this.obj = obj;
+  }
+}
 
 export default class GameSavingLoader {
   static async load() {
     const result = await read();
     const save = await json(result);
-    return JSON.parse(save);
+    // eslint-disable-next-line no-useless-escape
+    return new GameSaving(save.replace(',n', ',\"n'));
   }
 }
