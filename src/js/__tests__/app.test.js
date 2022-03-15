262cd4b2a8075ca38d7a8109b148e73a66ba9533
AnastasiaCymbalyuk <n.wolk@mail.ru>
2022-03-15 22:00:15 +0300

    async2(fixed3)

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,14 +2,14 @@ import GameSavingLoader from '../loader';
 import GameSaving from '../save';
 
 it('test', async () => {
-  const obj = JSON.stringify({
+  const obj = {
     id: 9,
     created: 1546300800,
     userInfo: {
       id: 1, name: 'Hitman', level: 10, points: 2000,
     },
-  });
-  const objNew = new GameSaving(JSON.parse(obj));
+  };
+  const objNew = new GameSaving(obj);
   const save = await GameSavingLoader.load();
   expect(save).toEqual(objNew);
 });
--- src/js/save.js
+++ src/js/save.js
@@ -1,7 +1,7 @@
 export default class GameSaving {
-  constructor(id, created, userInfo) {
-    this.id = id;
-    this.created = created;
-    this.userInfo = userInfo;
+  constructor(obj) {
+    this.id = obj.id;
+    this.created = obj.created;
+    this.userInfo = obj.userInfo;
   }
 }
